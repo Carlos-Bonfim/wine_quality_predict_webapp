e6ace9b309345c4930b8f5c8adeb33e54e7600b9
Carlos Bonfim <carlos_bonfim10@hotmail.com>
2020-05-15 22:44:50 -0300

    add handler.py

--- handler.py
+++ handler.py
@@ -44,5 +44,5 @@ def predict():
 if __name__ == '__main__':
 
 	# iniciando o flask
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='127.0.0.1', port=port, debug=True)
+	port = os.environ.get("PORT", 5000)
+	app.run(host='0.0.0.0', port=port, debug=True)
